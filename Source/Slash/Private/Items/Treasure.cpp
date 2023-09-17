cebf9e6f392d7ce9d69be0f0045165c0eff82b62
FrancescoC <francesco.capirci@gmail.com>
2023-09-17 16:51:11 +0300

    WIP - creating treasure to pickup

---
+++ Source/Slash/Private/Items/Treasure.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Treasure.h"
+#include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// not calling Super as we don't care about set overlapping item, treasure will be picked up right away
+	ASlashCharacter* SlashCharacter = Cast<ASlashCharacter>(OtherActor);
+
+	if (SlashCharacter)
+	{
+		if (PickupSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound,
+				GetActorLocation()
+			);
+		}
+		Destroy();
+	}
+}
